bf44fd4e6273e58f371d4aab2865c06d5ff21aa9
devshinon-br <rezendekeyla126@gmail.com>
2024-03-09 18:51:06 -0300

    chore: add improvements

--- js/main.js
+++ js/main.js
@@ -49,13 +49,25 @@ const getRandomPositions = (vet) => {
 document.addEventListener('DOMContentLoaded', () => {
     const vetAges = [10, 21, 31, 40];
    
+    // Question 1
     document.write(`<strong>Idades = ${vetAges}</strong>`);
     document.write(`<p>Soma das idades = ${sumAges(vetAges)}</p>`);
+
+    // Question 2
+
+    // a
     document.write(`<p>Média das idades = ${calculateAverage(vetAges)}</p>`)
+
+    // b
     document.write(`<p>Maior idade = ${findMaxAge(vetAges)}</p>`)
+
+    // c
     document.write(`<p>Idades ímpares = ${getOddAges(vetAges)}</p>`)
+
+    // d
     document.write(`<p>Todos são maiores de idade = ${allAreAdults(vetAges)}</p>`)
 
+    // e
     const userThreshold = prompt('Informe um valor para verificar se todas as idades são maiores ou iguais:');
     document.write(`<p>Todas as idades são maiores ou iguais a ${parseInt(userThreshold)}? =  ${allAgesGreaterOrEqual(vetAges, parseInt(userThreshold))}</p>`);
 
@@ -63,6 +75,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const randomAge = vetAges[randomIndex];
 
     document.write(`</p><strong> Idade aleatória escolhida = ${randomAge}</strong></p>`)
+    
+    // f
     document.write(`<p>Todas as idades maiores ou igual a ${randomAge} = ${displayAgesGreaterOrEqual(vetAges, randomAge)}</p>`)
+    
+    // g
     document.write(`<p>Média das idades maiores ou iguais a ${randomAge}= ${calculateAverageOfSelectedAges(vetAges, randomAge)}</p>`)
 });
